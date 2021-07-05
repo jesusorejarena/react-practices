1f3dcdeb6a66ad8e202c3b033043c23b1b120e45
jesusorejarena <jesusorejarena@gmail.com>
2021-07-05 18:30:42 -0400

    Respaldo

---
+++ MERNTask/servidor/routes/auth.js
@@ -0,0 +1,15 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const auth = require('../middleware/auth');
+const authController = require('../controllers/authController');
+
+// Iniciar sesion
+// api/auth
+router.post('/', authController.autenticarUsuario);
+
+// Obtiene el usuario autenticado
+router.get('/', auth, authController.usuarioAutenticado);
+
+module.exports = router;
